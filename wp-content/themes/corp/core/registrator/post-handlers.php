7f335ccf06bd8e38aa04bd664bf81fdcceeda267
chaitu.vela <chaitu.vella@gmail.com>
2014-04-28 12:09:54 +0530

    removing unwanted themes

--- wp-content/themes/corp/core/registrator/post-handlers.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-#Handler for feedbacks
-if (isset($_POST['sendthisfeedback'])) {
-	sendFeedback($_POST['senderemail'], $_POST['sendermessage'], $_POST['sendername'], $_POST['feedback_url']);
-}
-
-?>
